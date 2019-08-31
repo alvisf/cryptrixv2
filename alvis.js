165e971125ad6c37825b46483688841b929f9d0f
Alvis <falvis@gmail.com>
2019-08-31 14:40:00 +0530

    31/09/19

--- alvis.js
+++ alvis.js
@@ -1,56 +1,54 @@
-var childVal=null;
-var keyVal=null;
-
-
-
-
+var childVal = null;
+var keyVal = null;
+var lastNo = lastNum();
 var firebaseConfig = {
-    apiKey: "AIzaSyBFo7mBVgXTNmuKFI9ZDQ_yh64XKjEMAeA",
-    authDomain: "cryptrix-2k19.firebaseapp.com",
-    databaseURL: "https://cryptrix-2k19.firebaseio.com",
-    projectId: "cryptrix-2k19",
-    storageBucket: "cryptrix-2k19.appspot.com",
-    messagingSenderId: "1032074784863",
-    appId: "1:1032074784863:web:b1866501f96a498b"
-  };
-  firebase.initializeApp(firebaseConfig);
-  
-  var messagesRef = firebase.database().ref('RegisteredUsers');
+  apiKey: "AIzaSyBFo7mBVgXTNmuKFI9ZDQ_yh64XKjEMAeA",
+  authDomain: "cryptrix-2k19.firebaseapp.com",
+  databaseURL: "https://cryptrix-2k19.firebaseio.com",
+  projectId: "cryptrix-2k19",
+  storageBucket: "cryptrix-2k19.appspot.com",
+  messagingSenderId: "1032074784863",
+  appId: "1:1032074784863:web:b1866501f96a498b"
+};
+firebase.initializeApp(firebaseConfig);
 
+var messagesRef = firebase.database().ref("RegisteredUsers");
 
-  
-  
-  document.getElementById('alvissubmit').addEventListener('submit', saveMessage);
-  
-  
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
+document.getElementById("alvissubmit").addEventListener("submit", saveMessage);
 
-  function loadMessage(alviscollege,alvisemail,alvisgender,alvisregno,alvisphone,alvisname,alvisfood){
-    var newMessageRef = messagesRef.child("CRX"+childVal);
-    newMessageRef.set({
-      c:alviscollege,
-      e:alvisemail,
-      g:alvisgender,
-      id:"CRX"+childVal,
-      n:alvisregno,
-      p:alvisphone,
-      r:("1"==="1"),
-      u:alvisname,
-      v:alvisfood
-    });
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
 
-    
-    creDit();
-    // serverReq();
-    rDone();
-  }
+function loadMessage(
+  alviscollege,
+  alvisemail,
+  alvisgender,
+  alvisregno,
+  alvisphone,
+  alvisname,
+  alvisfood
+) {
+  var newMessageRef = messagesRef.child("CRX" + childVal + lastNo);
+  newMessageRef.set({
+    c: alviscollege,
+    e: alvisemail,
+    g: alvisgender,
+    id: "CRX" + childVal + lastNo,
+    n: alvisregno,
+    p: alvisphone,
+    r: "1" === "1",
+    u: alvisname,
+    v: alvisfood
+  });
 
-  function serverReq(){
+  creDit();
+  // serverReq();
+  rDone();
+}
 
-    
-    var xhttp = new XMLHttpRequest();
+function serverReq() {
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // document.getElementById("demo").innerHTML = this.responseText;
@@ -59,102 +57,126 @@ var firebaseConfig = {
   };
   xhttp.open("POST", "demo_post.asp", true);
   xhttp.send();
-  }
-  function delTop(){
-
-    var ref = firebase.database().ref('unqId');
-    ref.child(keyVal).remove();
-    // console.log(keyVal);
-
-   
-   }
-   function loadd(alviscollege,alvisemail,alvisgender,alvisregno,alvisphone,alvisname,alvisfood){
-    stackTop();
-
-    while(childVal!=null){
-      checkIfUserExists(childVal);
-      
-      if(!exists){
-        loadMessage(alviscollege,alvisemail,alvisgender,alvisregno,alvisphone,alvisname,alvisfood);
-        delTop();
-        
-      }
-      else{
-        loadd();
-
-      }
-      break;
-      }
-   }
-    function saveMessage(e){
-      e.preventDefault();
-      
-      var alviscollege = getInputVal('college');
-      var alvisemail = getInputVal('email');
-      var alvisgender = getInputVal('sex');
-      var alvisregno = getInputVal('regno');
-      var alvisphone = getInputVal('phone');
-      var alvisname = getInputVal('name');
-      var alvisfood = ("1"=== getInputVal('food').toString());
-
-      loadd(alviscollege,alvisemail,alvisgender,alvisregno,alvisphone,alvisname,alvisfood);
-//     var ref = firebase.database().ref('unqId');
-//     ref.orderByValue().limitToFirst(1).once("value", function(snapshot) {
-//       console.log(ref.child("unqId"));
-//       // snapshot.remove();
-//       // console.log(snapshot.val());
-//    }, function (error) {
-//       console.log("Error: " + error.code);
-//    });
-
-
-    }
-
-function saveVal(hah,lol){
-
-    childVal=hah;
-    keyVal=lol;
 }
-    function stackTop(){
-        var ref = firebase.database().ref('unqId');
-    ref.orderByValue().limitToFirst(1).once("value", function(snapshot) {
-        
+function delTop() {
+  var ref = firebase.database().ref("unqId");
+  ref.child(keyVal).remove();
+  // console.log(keyVal);
+}
+function loadd(
+  alviscollege,
+  alvisemail,
+  alvisgender,
+  alvisregno,
+  alvisphone,
+  alvisname,
+  alvisfood
+) {
+  stackTop();
+
+  while (childVal != null) {
+    // checkIfUserExists("CRX" + childVal);
+
+    if (!exists) {
+      loadMessage(
+        alviscollege,
+        alvisemail,
+        alvisgender,
+        alvisregno,
+        alvisphone,
+        alvisname,
+        alvisfood
+      );
+      delTop();
+    } else {
+      loadd();
+    }
+    break;
+  }
+}
+function saveMessage(e) {
+  e.preventDefault();
+
+  var alviscollege = getInputVal("college");
+  var alvisemail = getInputVal("email");
+  var alvisgender = getInputVal("sex");
+  var alvisregno = getInputVal("regno");
+  var alvisphone = getInputVal("phone");
+  var alvisname = getInputVal("name");
+  var alvisfood = "1" === getInputVal("food").toString();
+
+  loadd(
+    alviscollege,
+    alvisemail,
+    alvisgender,
+    alvisregno,
+    alvisphone,
+    alvisname,
+    alvisfood
+  );
+  //     var ref = firebase.database().ref('unqId');
+  //     ref.orderByValue().limitToFirst(1).once("value", function(snapshot) {
+  //       console.log(ref.child("unqId"));
+  //       // snapshot.remove();
+  //       // console.log(snapshot.val());
+  //    }, function (error) {
+  //       console.log("Error: " + error.code);
+  //    });
+}
+
+function saveVal(hah, lol) {
+  childVal = hah;
+  keyVal = lol;
+}
+function stackTop() {
+  var ref = firebase.database().ref("unqId");
+  ref
+    .orderByValue()
+    .limitToFirst(1)
+    .once(
+      "value",
+      function(snapshot) {
         snapshot.forEach(function(child) {
-                saveVal(child.val().toString(),child.key)
-          });
+          saveVal(child.val().toString(), child.key);
+        });
 
+        //   console.log(snapshot.child.val());
+      },
+      function(error) {
+        console.log("Error: " + error.code);
+      }
+    );
+}
 
-    //   console.log(snapshot.child.val());
-   }, function (error) {
-      console.log("Error: " + error.code);
-   });
-   
-    }
+function creDit() {
+  console.log("made by Alvis.F");
+  console.log("made by Daniel D");
+  console.log("made by Dennis.F");
+}
 
-    function creDit(){
-        
-            console.log("made by Alvis.F");
-            console.log("made by Daniel D");
-            console.log("made by Dennis.F");
-        
-    }
+function rDone() {
+  $(document).ready(function() {
+    $("#alvisdis").html(
+      '<h1><br><center>SUCCESSFULLY REGISTERED!</center></h1>\n<h1><br><center>THANK YOU FOR REGISTERING!</center></h1>\n<h1 style="font-size:5vw;">\n<br>\n<center>YOUR CRYPTRIX 2K19 ID IS:\n<span style="color: red">CRX' +
+        childVal +
+        lastNo +
+        "</span>\n</center>\n</h1>\n<h1>\n<br>\n<center>PLEASE BRING THIS CRYPTRIX ID ON THE DAY OF THE SYMPOSIUM</center>\n</h1>\n<h1>\n<br>\n<center>A CONFIRMATION MAIL HAS BEEN SENT TO YOUR EMAIL ADDRESS</center>\n</h1>\n"
+    );
+  });
+}
+var exists;
 
-    function rDone(){
+// function checkIfUserExists(userId) {
+//   var usersRef = firebase.database().ref("RegisteredUsers");
+//   usersRef.child(userId).once("value", function(snapshot) {
+//     exists = snapshot.exists();
+//     console.log(exists);
+//   });
+// }
 
-      $(document).ready(function(){
-           $('#alvisdis').html('<h1><br><center>SUCCESSFULLY REGISTERED!</center></h1>\n<h1><br><center>THANK YOU FOR REGISTERING!</center></h1>\n<h1 style="font-size:5vw;">\n<br>\n<center>YOUR CRYPTRIX 2K19 ID IS:\n<span style="color: red">CRX'+childVal+'</span>\n</center>\n</h1>\n<h1>\n<br>\n<center>PLEASE BRING THIS CRYPTRIX ID ON THE DAY OF THE SYMPOSIUM</center>\n</h1>\n<h1>\n<br>\n<center>A CONFIRMATION MAIL HAS BEEN SENT TO YOUR EMAIL ADDRESS</center>\n</h1>\n');
-
-      });
-
-
-  }
-  var exists;
-
-  function checkIfUserExists(userId) {
-
-    var usersRef = firebase.database().ref('RegisteredUsers');
-    usersRef.child(userId).once('value', function(snapshot) {
-      exists = (snapshot.exists());
-      console.log(exists);
-    });
-  }
+function lastNum() {
+  var min = 0;
+  var max = 9;
+  var random = Math.floor(Math.random() * (+max - +min)) + +min;
+  return random;
+}
--- config.js
+++ config.js
@@ -1,93 +1,86 @@
 "use strict";
 // Wiresphere config file
 // Do not delete anything from this file!
-var colp =["#800080","003d4f"];
+var colp = ["#800080", "003d4f"];
 var WiresphereConfig = {
-	Background: {
-		SolidBg: {							
-			Color: '#003d4f'				// Solid background color
-		},
-		
-		Picture: {
-			Enabled: false,					// Background picture true/false
-			Opacity: '0.07'					// Background picture opacity
-		},
-		
-		Gradient: {
-			Enabled: true,					// Background animated gradient true/false	
-			StopAColor : '0,204,255',		// Background animated gradient Stop A
-			StopBColor : '0,204,255',		// Background animated gradient Stop B
-			Opacity : 0.2					// Background animated gradient opacity
-		},
-		
-		
-		Sphere3D: {
-			lineColor: 0xffffff,			// Color of sphere outline
-			lineOpacity: 0.5,				// Opacity of front face of sphere
-			backlineOpacity: 0.05,			// Opacity of backface of sphere
-			particleColor: 0xFFD700,		// Color of particles
-			particleOpacity: 0.5,			// Opacity of particles
-			particleSize: 10,				// Size of particles
-			particlesAmmount: 750,			// Ammount of particles
-			moveSpeed: 0.07,				// Speed of particles
-			cameraXMoveMax: 1.5,			// Mouse movement max rotation
-			cameraYMoveMax: 0.75,			// Mouse movement max rotation
-			cameraXMoveElastic: 0.02,		// Mouse movement smoothness
-			cameraYMoveElastic: 0.02		// Mouse movement smoothness
-		},
-		
-		Noise: {
-			Enabled: true,					// Background noise true/false
-			Opacity: '0.05'					// Background noise opacity
-		}
-		
-	},
-	
-	Colors: {
-		JSOverride: true,					// Set to false if you want to tweak CSS
-		
-		Tagline: {							// Tagline
-			FirstLine: '#fff',				// Color of first line of tagline
-			SecondLine: '#fff'				// Color of second line of tagline
-		},
-		
-		Buttons: {							// Buttons
-			Outline: '#fff',				// Color of buttons outline
-			Text: '#fff',					// Color of buttons text
-			TextHover: 'rgba(255,255,255,0.5)'		// Color of buttons text on hover
-		},
-		
-		SideContent: {						// Side content 
-			Background: '#000',				// Background of side content 
-			Text: '#FFF',					// Text color of side content 
-			ContactIcons: '#DDD',			// Color of contact icons
-		}
-		
-	},
-	
-	Map: {
-		Longtitude: -73.946,				// X Map location 
-		Latitude: 40.674,					// X Map location
-		Style: [							// X Map style - refer to Google Maps for more details
-		  {
-			"stylers": [
-			  { "saturation": -100 },
-			  { "lightness": 62 },
-			  { "gamma": 0.47 }
-			]
-		  },{
-			"elementType": "labels.text",
-			"stylers": [
-			  { "color": "#ffffff" },
-			  { "visibility": "simplified" }
-			]
-		  },{
-			"elementType": "labels.icon",
-			"stylers": [
-			  { "gamma": 2.17 }
-			]
-		  }
-		]
-	}
-	
-}
+  Background: {
+    SolidBg: {
+      Color: "#003d4f" // Solid background color
+    },
+
+    Picture: {
+      Enabled: false, // Background picture true/false
+      Opacity: "0.07" // Background picture opacity
+    },
+
+    Gradient: {
+      Enabled: true, // Background animated gradient true/false
+      StopAColor: "0,204,255", // Background animated gradient Stop A
+      StopBColor: "0,204,255", // Background animated gradient Stop B
+      Opacity: 0.2 // Background animated gradient opacity
+    },
+
+    Sphere3D: {
+      lineColor: 0xffffff, // Color of sphere outline
+      lineOpacity: 0.5, // Opacity of front face of sphere
+      backlineOpacity: 0.05, // Opacity of backface of sphere
+      particleColor: 0xffd700, // Color of particles
+      particleOpacity: 0.5, // Opacity of particles
+      particleSize: 10, // Size of particles
+      particlesAmmount: 750, // Ammount of particles
+      moveSpeed: 0.07, // Speed of particles
+      cameraXMoveMax: 1.5, // Mouse movement max rotation
+      cameraYMoveMax: 0.75, // Mouse movement max rotation
+      cameraXMoveElastic: 0.02, // Mouse movement smoothness
+      cameraYMoveElastic: 0.02 // Mouse movement smoothness
+    },
+
+    Noise: {
+      Enabled: true, // Background noise true/false
+      Opacity: "0.05" // Background noise opacity
+    }
+  },
+
+  Colors: {
+    JSOverride: true, // Set to false if you want to tweak CSS
+
+    Tagline: {
+      // Tagline
+      FirstLine: "#fff", // Color of first line of tagline
+      SecondLine: "#fff" // Color of second line of tagline
+    },
+
+    Buttons: {
+      // Buttons
+      Outline: "#fff", // Color of buttons outline
+      Text: "#fff", // Color of buttons text
+      TextHover: "rgba(255,255,255,0.5)" // Color of buttons text on hover
+    },
+
+    SideContent: {
+      // Side content
+      Background: "#000", // Background of side content
+      Text: "#FFF", // Text color of side content
+      ContactIcons: "#DDD" // Color of contact icons
+    }
+  },
+
+  Map: {
+    Longtitude: -73.946, // X Map location
+    Latitude: 40.674, // X Map location
+    Style: [
+      // X Map style - refer to Google Maps for more details
+      {
+        stylers: [{ saturation: -100 }, { lightness: 62 }, { gamma: 0.47 }]
+      },
+      {
+        elementType: "labels.text",
+        stylers: [{ color: "#ffffff" }, { visibility: "simplified" }]
+      },
+      {
+        elementType: "labels.icon",
+        stylers: [{ gamma: 2.17 }]
+      }
+    ]
+  }
+};
